4a4f50fed28bcc131fef2bf35cae9fb1d225df51
Andrei Kozsar <aikozsar@luxoft.com>
2017-09-12 12:08:34 +0300

    Adding calloc implementation

---
+++ Exercise8-6/exer8-6.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void *mycalloc(unsigned, unsigned);
+
+void *mycalloc(unsigned n,unsigned size)
+{
+    unsigned i,nb;
+    char *p,*q;
+
+    nb = n * size;
+    
+    if((p = q = malloc(nb)) != NULL)
+        for(i = 0; i <nb; i++)
+            *p++ = 0;
+    return q;
+
+}
+
+int main(int argc, char *argv[]) {
+    int *p = NULL;
+    int i = 0;
+
+    p = mycalloc(100, sizeof *p);
+    if (p == NULL) {
+        printf("mycalloc returned NULL");
+
+    } 
+    
+    free(p);
+    return 0;
+}
